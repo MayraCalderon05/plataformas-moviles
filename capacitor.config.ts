25596ccb846a0275bb7aa36097f6eb2764c37eb4
Mayra_Calderon <may2005_cc@hotmail.com>
2024-06-13 15:58:26 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'miFistApp',
+  webDir: 'www'
+};
+
+export default config;
