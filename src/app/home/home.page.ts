519051faab1b9876680ecb46265a7c60f7721457
MayraCalderon05 <may2005_cc@hotmail.com>
2024-09-04 17:43:21 -0300

    Creamos el proyecto; editamos el header poniendole un titulo; creamos un botón con una acción que abra el ion-action-sheet y agregamos los botones para que se muestre en el menú; luego agregamos un alerta extra para preguntar si estaría seguro de realizar dicha acción

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,40 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  public mostrar: boolean = false;
+  public mostrarAlert: boolean = false;
+  public botonesAlert = ["Sí","Cancel"]
+
+  public botones = [
+    {
+      text:"Delete",
+      icon:"trash",
+      //?se pueden agregar eventos directamente al objeto
+      handler: () =>(
+        //?para poder llamar propiedades y metodos dentro de la misma clase de utiliza el this.
+        this.setAlert(true)
+      )
+    },
+    {
+      text:"Save",
+      icon:"save",
+    },
+    {
+      text:"Cancel",
+      icon:"close",
+    }
+  ];
 
   constructor() {}
+  
+  //? set action es cambiar acción
+  setAction(estado: boolean){
+    //alert("Hola, has abrido algo");
+    //*ion action sheet es una acción que se va ejecutar con opciones
+    this.mostrar = estado;
+  }
+  setAlert(estado: boolean){
+    this.mostrarAlert = estado;
+  }
 
 }
